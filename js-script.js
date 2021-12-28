507e702fdeb72f175c39de7faed2fd51b52e4a37
Cody <whearcody@gmail.com>
2021-12-28 17:54:13 +0000

    2

--- js-script.js
+++ js-script.js
@@ -15,45 +15,47 @@ const gameBoard = (() => {
     let currentPlayer = document.getElementById("playerShower");
     currentPlayer.textContent = "Current player: " + player1.name;
     player1.playing = true;
+    const xaxis = 3;
+    const yaxis = 3;
 
-    for (let i = 0; i < 9; i++) {
-        let square = document.createElement("div");
-        const content = document.createTextNode("");
-        square.className = "square";
-        square.id = "square" + i;
-        square.appendChild(content);
-        square.addEventListener("click", function() {
-            if (square.textContent !== "") {
-                return;
-            }
+    
+    for (let i = 0; i < xaxis; i++) {
+        for (let j = 0; j < yaxis; j++) {
+            let square = document.createElement("div");
+            const content = document.createTextNode("");
+            square.className = "square";
+            square.id = "square" + i + j;
+            square.appendChild(content);
+            square.addEventListener("click", function() {
+                if (square.textContent !== "") {
+                    return;
+                }
+                if (player1.playing) {
+                    square.textContent = "X";
+                } else if (player2.playing) {
+                    square.textContent = "O";
+                }
 
-            if (player1.playing) {
-                square.textContent = "X";
-            } else if (player2.playing) {
-                square.textContent = "O";
-            }
-
-            if (player1.playing) {
-                currentPlayer.textContent = "Current player: " + player2.name;
-                player1.playing = false;
-                player2.playing = true;
-            } else if (player2.playing) {
-                currentPlayer.textContent = "Current player: " + player1.name;
-                player1.playing = true;
-                player2.playing = false;
-            }
-            if (displayController.gameOver() == "game over") {
-                return;
-            }
-        });
-        board.appendChild(square);
-    }
-    return; 
+                if (player1.playing) {
+                    currentPlayer.textContent = "Current player: " + player2.name;
+                    player1.playing = false;
+                    player2.playing = true;
+                    if (displayController.gameOver() == "done") {return};
+                }
+                setTimeout(displayController.computerTurn, 500);
+                displayController.gameOver();
+                
+            });
+            board.appendChild(square);
+    }}
+    let square = 4;
+    return {square};
 })();
 
 const displayController = (() => {
+    let board = document.getElementById("GB").childNodes;
+    let currentPlayer = document.getElementById("playerShower");
     function gameOver() {
-        let board = document.getElementById("GB").childNodes;
         if (((board[0].textContent == board[1].textContent && board[1].textContent == board[2].textContent) && board[0].textContent != "")
         || ((board[0].textContent == board[4].textContent && board[4].textContent == board[8].textContent)  && board[0].textContent != "")
         || ((board[0].textContent == board[3].textContent && board[3].textContent == board[6].textContent)  && board[0].textContent != "")
@@ -64,13 +66,27 @@ const displayController = (() => {
         || ((board[6].textContent == board[4].textContent && board[4].textContent == board[2].textContent) && board[6].textContent != "")) {
             if (player2.playing) {
                 console.log(player1.name + " has won!");
+                return "done";
             } else if (player1.playing) {
                 console.log(player2.name + " has won!");
+                return "done";
             }
             return "game over";
         }
     }
+    function computerTurn() {
+        let choice1 = Math.floor(Math.random() * 8);
+        if (board[choice1].textContent == "") {
+            board[choice1].textContent = "O";
+        } else {
+            computerTurn();
+        }
+        currentPlayer.textContent = "Current player: " + player1.name;
+        player2.playing = false;
+        player1.playing = true;
 
-    return {gameOver};
+    }
+
+    return {gameOver, computerTurn};
 })();
 
