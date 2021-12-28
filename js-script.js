67763b43b70be4dbf028129e651a6761817b2560
Cody <whearcody@gmail.com>
2021-12-28 12:07:41 +0000

    Made the boxes clickable only once, added players as a feature with different signs (X or O) and displayed some more info

--- js-script.js
+++ js-script.js
@@ -1,12 +1,49 @@
+const players = (name, XorO) => {
+    let playing = false;
+    return {name, playing, XorO};
+};
+
+const player1 = players("Cody", "X");
+const player2 = players("AI", "O");
+
 const gameBoard = (() => {
     let board = document.getElementById("GB");
-    
+    let identifier1 = document.getElementById("identifier1");
+    identifier1.textContent = player1.name + " playing as: " + player1.XorO;
+    let identifier2 = document.getElementById("identifier2");
+    identifier2.textContent = player2.name + " playing as: " + player2.XorO;
+    let currentPlayer = document.getElementById("playerShower");
+    currentPlayer.textContent = "Current player: " + player1.name;
+    player1.playing = true;
+
     for (let i = 0; i < 9; i++) {
         let square = document.createElement("div");
-        const content = document.createTextNode("X");
+        const content = document.createTextNode("");
         square.className = "square";
         square.id = "square" + i;
         square.appendChild(content);
+        square.addEventListener("click", function() {
+            if (square.textContent !== "") {
+                return;
+            }
+
+            if (player1.playing) {
+                square.textContent = "X";
+            } else if (player2.playing) {
+                square.textContent = "O";
+            }
+
+            if (player1.playing) {
+                currentPlayer.textContent = "Current player: " + player2.name;
+                player1.playing = false;
+                player2.playing = true;
+            } else if (player2.playing) {
+                currentPlayer.textContent = "Current player: " + player1.name;
+                player1.playing = true;
+                player2.playing = false;
+            }
+            
+        });
         board.appendChild(square);
     }
 
@@ -14,16 +51,8 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() => {
-
+    
 
     return;
 })();
 
-const players = (name) => {
-  
-
-    return {name};
-};
-
-const player1 = players("Cody");
-const player2 = players("AI");
